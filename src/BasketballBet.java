320227279dee31d798104893ca5793d4523c1cb3
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-11-14 20:41:07 +0200

    Create BasketballBet.java

---
+++ src/BasketballBet.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class BasketballBet extends Bet{
+    // The class contains the available outcomes of a basketball match.
+    // 1: The first team wins, 2: The second team wins
+    // The list of available options is immutable.
+    private static final List<String> choices = List.of("1", "2");
+    // The outcome of the bet
+    private final String outcome;
+
+    public BasketballBet(String game, double odd, String outcome){
+        super(game, odd);
+        this.outcome = outcome;
+    }
+
+    public String getOutcome() {
+        return outcome;
+    }
+
+    public static List<String> getChoices() {
+        return choices;
+    }
+}
