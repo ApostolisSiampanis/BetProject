3cf6c0c536ba91617c31908ef9eb50ad7aa2256d
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-11-16 10:57:58 +0200

    Create Bet.java

--- src/Bet.java
+++ src/Bet.java
@@ -1,4 +1,4 @@
-public class Bet {
+public abstract class Bet {
     // The name of the game. We assume that the name is unique.
     private final String game;
     // The odd of the bet. We assume the same odd, regardless of which team will win
