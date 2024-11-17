5331b487e45c6c1d4433335b9cfe8e0f060dd895
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-11-17 12:19:58 +0200

    Update FootballBet.java and BasketballBet.java

--- src/bet/BasketballBet.java
+++ src/bet/BasketballBet.java
@@ -7,19 +7,8 @@ public final class BasketballBet extends Bet{
     // 1: The first team wins, 2: The second team wins
     // The list of available options is immutable.
     private static final List<String> choices = List.of("1", "2");
-    // The outcome of the bet
-    private final String outcome;
 
-    public BasketballBet(String game, double odd, String outcome){
-        super(game, odd);
-        this.outcome = outcome;
-    }
-
-    public String getOutcome() {
-        return outcome;
-    }
-
-    public static List<String> getChoices() {
-        return choices;
+    public BasketballBet(String game, double odd){
+        super(game, odd, choices);
     }
 }
