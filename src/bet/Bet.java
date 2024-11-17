13250e3a5a24ab647e6bf833de70fbab4edf8f3d
Apostolis Siampanis <apostolissiabanis@hotmail.com>
2024-11-17 12:19:36 +0200

    Update Bet.java

--- src/bet/Bet.java
+++ src/bet/Bet.java
@@ -1,20 +1,41 @@
 package bet;
 
+import java.util.List;
+
 public abstract sealed class Bet permits BasketballBet, FootballBet {
     // The name of the game. We assume that the name is unique.
     private final String game;
     // The odd of the bet. We assume the same odd, regardless of which team will win
     private final double odd;
-    public Bet(String game, double odd) {
+    // The available choices of the bet
+    private final List<String> choices;
+    // The outcome of the bet
+    private final String outcome;
+
+    public Bet(String game, double odd, List<String> choices) {
         this.game = game;
         this.odd = odd;
+        this.choices = choices;
+        this.outcome = "";
     }
 
+    // Returns the name of the game
     public String getGame() {
         return game;
     }
 
+    // Returns the odd of the bet
     public double getOdd() {
         return odd;
+    }
+
+    // Returns the outcome of the bet
+    public String getOutcome() {
+        return outcome;
+    }
+
+    // Returns the available choices of the bet
+    public List<String> getChoices() {
+        return choices;
     }
 }
